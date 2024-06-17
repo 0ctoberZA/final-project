33de3e433090c16928f91b9639fee764220460bc
0ctoberZA <tsakanilindsay@gmail.com>
2024-06-17 10:40:27 +0200

    server done

--- script.js
+++ script.js
@@ -1,5 +1,167 @@
 "use script";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const signupForm = document.getElementById("signup-form");
+  const loginForm = document.getElementById("login-form");
+  const logoutForm = document.getElementById("logout-form");
+
+  signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(signupForm);
+    const username = formData.get("username");
+    const password = formData.get("password");
+    const email = formData.get("email");
+    const full_name = formData.get("full_name");
+
+    try {
+      const response = await fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password, email, full_name }),
+      });
+      if (response.ok) {
+        alert("Signup successful.");
+      } else {
+        alert("Signup failed, try again!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(loginForm);
+    const username = formData.get("username");
+    const password = formData.get("password");
+    try {
+      const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        alert("Login Successful");
+      } else {
+        alert("Wrong password or username");
+      }
+    } catch (error) {
+      console.error("Error", error);
+    }
+  });
+
+  logoutForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/logout", {
+        method: "POST",
+      });
+      if (response.ok) {
+        alert("Logout successful");
+      } else {
+        alert("failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
+
+  serviceForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(serviceForm);
+    const selectedServices = formData.getAll("service");
+    try {
+      const response = await fetch("/select-services", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ selectedServices }),
+      });
+      if (response.ok) {
+        alert("Service selection successful");
+      } else {
+        alert("Service selection failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
+
+  //fetch and display selected services
+  fetchSelectedServices();
+});
+
+async function fetchSelectedServices() {
+  try {
+    const response = await fetch("/selected-services");
+    if (response.ok) {
+      const selectedServices = await response.json();
+      displaySelectedServices(selectedServices);
+    } else {
+      console.error("Failed to fetch selected services");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function displaySelectedServices(selectedServices) {
+  const selectedServicesList = document.getElementById(
+    "selected-services-list"
+  );
+  selectedServicesList.innerHTML = "";
+
+  selectedServices.forEach((service) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = service.name;
+    selectedServicesList.appendChild(listItem);
+  });
+}
+
+if (window.location.pathname === "/service-content") {
+  fetchSelectedServices();
+}
+
+function fetchServiceContent() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const serviceId = urlParams.get("id");
+
+  fetch(`/service/${serviceId}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      displaySelectedServices(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching service content:", error);
+    });
+}
+
+function displayServiceContent(serviceContent) {
+  const serviceNameElement = document.getElementById("service-name");
+
+  serviceNameElement.textContent = serviceContent.name;
+  const serviceContentElement = document.getElementById("service-content");
+  serviceContentElement.innerHTML = "";
+
+  serviceContent.select.forEach((selected) => {
+    const selectedSection = document.createElement("section");
+    selectedSection.innerHTML = `<h2>${selected.title} </h2>
+  <p>${selected.description}</p>
+  `;
+    serviceContentElement.appendChild(selectedSection);
+  });
+}
+
 const header1 = document.querySelector(".header1");
 
 function displayText() {
@@ -28,7 +190,7 @@ message.style.height =
 console.log(message.style.backgroundColor);
 
 const bookService = document.querySelector(".btn--book");
-const form = document.querySelector(".form");
+const form = document.querySelector("#signup-form");
 
 bookService.addEventListener("click", function (e) {
   const s1coords = form.getBoundingClientRect();
@@ -44,6 +206,8 @@ document.querySelectorAll(".link").forEach(function (el) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// Service order form submission
 
 /*
 document.querySelector(".link").addEventListener("click", function (e) {
--- server.js
+++ server.js
@@ -4,9 +4,226 @@ const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const { check, validationResult } = require("express-validator");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
-app.get("/", function (req, res) {
-  res.send(dirname + "/index.html");
+const PORT = process.env.PORT;
+
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.post("/login", (req, res) => {
+  if (validLogin) {
+    req.session.username = username;
+    req.session.email = email;
+    req.session.successfulLogin = true;
+  } else {
+    res.redirect("/login");
+  }
+});
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// connect to mysql
+connection.connect((err) => {
+  if (err) {
+    console.error(" Error connecting to MySql: " + err.stack);
+    return;
+  }
+  console.log(" Connected to MySql as id " + connection.threadId);
+});
+
+app.use(express.static(__dirname));
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//secure routes
+const isAuthenticated = (req, res) => {
+  if (req.session.successfulLogin) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const User = {
+  tableName: "clients",
+  createUser: function (newUser, callback) {
+    connection.query(
+      "INSERT INTO " + this.tableName + " SET ? ",
+      newUser,
+      callback
+    );
+  },
+  getUserByEmail: function (email, callback) {
+    connection.query(
+      " SELECT * FROM " + this.tableName + " WHERE email = ? ",
+      email,
+      callback
+    );
+  },
+
+  getUserByUsername: function (username, callback) {
+    connection.query(
+      " SELECT * FROM " + this.tableName + " WHERE username = ? ",
+      username,
+      callback
+    );
+  },
+};
+
+app.post(
+  "/signup",
+  [
+    check("email").isEmail(),
+    check("username").isAlphanumeric().withMessage("username"),
+
+    check("email").custom(async (value) => {
+      const user = await User.getUserByEmail(value);
+      if (user) {
+        throw new Error("Email already exists!");
+      }
+    }),
+    check("username").custom(async (value) => {
+      const user = await User.getUserByUsername(value);
+      if (user) {
+        throw new Error("Username already exists");
+      }
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    const newUser = {
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword,
+      full_name: req.body.full_name,
+    };
+
+    // insert user to mysql
+    User.createUser(newUser, (error, results, fields) => {
+      if (error) {
+        console.error("Error inserting user: " + error.message);
+        return res.status(500).json({ error: error.message });
+      }
+      console.log("Inserting a new user with id " + results.insertId);
+      res.status(201).json(newUser);
+    });
+  }
+);
+// login route
+app.post("/login", isAuthenticated, (req, res) => {
+  const { username, password } = req.body;
+
+  connection.query(
+    " SELECT * FROM clients WHERE username = ? ",
+    [username],
+    (err, results) => {
+      if (err) throw err;
+      if (results.length === 0) {
+        res.status(401).send("Wrong username or password");
+      } else {
+        const user = results[0];
+        // compare passwords
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            req.session.user = user;
+            res.send("Login successful");
+          } else {
+            res.status(401).send("Wrong username or password");
+          }
+        });
+      }
+    }
+  );
+});
+
+// logout route
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
+// Service selection route
+app.post("/select-services", (req, res) => {
+  const { selectedServices } = req.body;
+  const clientId = req.session.user.id;
+});
+
+// insert selected services into database
+const sql = "INSERT INTO services (clients_Id, services_Id) VALUES ? ";
+const values = selectedServices.map((servicesId) => [clientsId, servicesId]);
+connection.query(sql, [values], (err, result) => {
+  if (err) {
+    console.error("Error inserting selected services:", err);
+    res.status(500).send("Failed to select services");
+  } else {
+    res.status(200).send("Services selected successfully");
+  }
+});
+
+// fetch selected service
+app.get("/selected-services", (req, res) => {
+  const clientsId = req.session.user.id;
+
+  const sql =
+    " SELECT * FROM services INNER JOIN user_services ON services.id = user_services.services_id WHERE user_services.user_id = ? ";
+  connection.query(sql, [clientsId], (err, results) => {
+    if (err) {
+      console.error("Error fetching selected services:", err);
+      res.status(500).send("Failed to fetch selected services");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+// retrieve services
+app.get("/service/:id", (req, res) => {
+  const servicesId = req.params.id;
+  const sql = "SELECT * FROM services WHERE id = ? ";
+  db.query(sql, [servicesId], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.json(result);
+  });
+});
+
+// start server
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
